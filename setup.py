f1878cc8bf21f7ae0ae59d059f77d3d5999ae298
Likhitha Reddy <likhithareddy.1996@gmail.com>
2025-08-14 15:42:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
 	name='project1',
 	version='1.0',
-	author='Harsha Vardhan',
-	authour_email='Donkada.H.Vardhan-1@ou.edu',
+	author='Likhitha Gundre',
+	authour_email='likhitha@ou.edu',
 	packages=find_packages(exclude=('tests', 'docs')),
 	setup_requires=['pytest-runner'],
 	tests_require=['pytest']	
